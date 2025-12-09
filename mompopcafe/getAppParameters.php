8f1b27c527f4360940f523666f24359531f85f8d
yabadabadoo-27 <85814369+abcd-123-hack@users.noreply.github.com>
2025-12-09 10:20:03 +0530

    Update getAppParameters.php

--- mompopcafe/getAppParameters.php
+++ mompopcafe/getAppParameters.php
@@ -4,7 +4,7 @@
   $showServerInfo = "false";
   $timeZone = "America/New_York";
   $currency = "$";
-  $db_url = "mysql";
+  $db_url = "192.168.50.128";
   $db_name = "mom_pop_db";
   $db_user = "web";
   $db_password = "web123";
@@ -12,6 +12,7 @@
   // Display the server metadata information if the showServerInfo parameter is true.
   //include('serverInfo.php');
 ?>
+
 
 
 
